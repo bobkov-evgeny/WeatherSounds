b2b5dd1d5e098ce8ce73f7723b499c132336e79d
Eugene <freeedayy@gmail.com>
2023-02-26 16:13:01 +0300

    first task implementation

--- src/index.ts
+++ src/index.ts
@@ -60,3 +60,25 @@ weatherList.forEach(weatherItem => {
     weatherItemsContainerElement.appendChild(newWeatherElement);
 });
 
+
+
+// ----------------- Typescript module homework
+
+interface ITotalPrice {
+    price: number;
+    discount: number;
+    isInstallment: boolean;
+    months: number;
+}
+const totalPrice = ({ price, discount, isInstallment, months }: ITotalPrice): number => {
+    const discountPrice = price * ((100 - discount) / 100);
+
+    if(isInstallment) {
+        return discountPrice / months;
+    } else {
+        return discountPrice;
+    }
+};
+
+totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+// 6250
